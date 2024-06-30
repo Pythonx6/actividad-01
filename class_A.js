1ada48f1194e3c1b015197150bfaaf219050d20c
Pythonx6 <aortiz8235@stu.nuc.edu>
2024-06-30 19:02:10 -0400

    Añade la clase A con los atributos foo y bar

---
+++ class_A.js
@@ -0,0 +1,11 @@
+class A {
+  constructor(foo, bar) {
+    this.foo = foo;
+    this.bar = bar;
+  }
+}
+
+// Puedes agregar más lógica aquí si es necesario
+
+// Exporta la clase A para poder usarla en otros archivos
+module.exports = A;
