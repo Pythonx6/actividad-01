8e2bf3c6470b06aa00c7dd7987874f1edd8ed903
Pythonx6 <aortiz8235@stu.nuc.edu>
2024-07-01 12:25:27 -0400

    Añadida clase C con atributos especificados

---
+++ class_C.js
@@ -0,0 +1,18 @@
+// Definición de la clase C
+class C {
+  constructor() {
+    this.nombre = "Alexander";
+    this.apellido = "Ortiz";
+    this.profesion = "Estudiante del Master Full Stack";
+    this.edad = 30;
+  }
+}
+
+// Creación de una instancia de la clase C
+let instanciaC = new C();
+
+// Imprimir los atributos en la consola
+console.log("Nombre:", instanciaC.nombre);
+console.log("Apellido:", instanciaC.apellido);
+console.log("Profesión:", instanciaC.profesion);
+console.log("Edad:", instanciaC.edad);
